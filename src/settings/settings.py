48542bf86aaa31462958562daaa237bd02a1be51
slladaltp <glasha123480@gmail.com>
2022-04-23 19:55:30 +0300

    Templates Added

--- src/settings/settings.py
+++ src/settings/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'homework.apps.StudentsConfig',
+    'homework',
 ]
 
 MIDDLEWARE = [
